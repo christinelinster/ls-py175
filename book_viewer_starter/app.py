107515e203ed3d2a1139015346dc34a977c1b1f2
Christine Lin <lin.christine19@gmail.com>
2025-09-02 13:36:36 -0400

    format paragraphs with jinja_env.filters

--- book_viewer_starter/app.py
+++ book_viewer_starter/app.py
@@ -1,29 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+from markupsafe import escape 
+import re
 
 app = Flask(__name__)
 
+def in_paragraphs(text):
+    paragraphs = text.split("\n\n")
+    formatted_paragraph = [
+        f'<p>{paragraph}</p>'
+        for paragraph in paragraphs
+        if paragraph
+    ]
+    return ''.join(formatted_paragraph)
+
+app.jinja_env.filters['in_paragraphs'] = in_paragraphs
+
+@app.before_request
+def load_contents():
+    with open('book_viewer/data/toc.txt', 'r') as file:
+        g.contents = file.readlines()
+
+
 @app.route("/")
 def index():
-    with open('book_viewer/data/toc.txt', 'r') as file:
-        contents = file.readlines()
+    return render_template("home.html", contents=g.contents)
 
-    return render_template("home.html", contents=contents)
-
-@app.route("/chapters/1")
-def chapter1():
-    chapter_title = "Chapter 1"
-    with open('book_viewer/data/toc.txt' ,'r') as file:
-        contents = file.readlines()
+@app.route("/chapters/<page_num>")
+def chapter(page_num):
+    chapter_name = g.contents[int(page_num) - 1]
+    chapter_title = f"Chapter {page_num}: {chapter_name}"
     
-    with open('book_viewer/data/chp1.txt', 'r') as file:
+    with open(f'book_viewer/data/chp{page_num}.txt', 'r') as file:
         chapter = file.read()
+
 
     return render_template('chapter.html', 
                            chapter_title=chapter_title,
-                           contents=contents,
+                           contents=g.contents,
                            chapter=chapter)
-
-
 
 
 if __name__ == "__main__":
